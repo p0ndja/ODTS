144616579134bd40d1869a1a6613b462da98a8ce
PondJaᵀᴴ <palapon2545@gmail.com>
2021-04-01 22:33:02 +0700

    Update Link 🔗

--- static/functions/sidenav.php
+++ static/functions/sidenav.php
@@ -1,11 +1,12 @@
-<div class="bg-white z-depth-2" id="sidebar-wrapper">
+<div class="bg-white z-depth-2 " id="sidebar-wrapper" style="max-width: 240px; ">
     <div class="sidebar-heading bg-white">
         <img src="../static/elements/logo/android-icon-192x192.png" width="192"/>
     </div>
-    <h5 class="text-center mb-3 text-pharm font-weight-bold">งานเภสัชกรรม<br>โรงพยาบาลศรีนครินทร์</h5>
+    <div class="text-center mb-5"><h5 class="text-pharm font-weight-bold">ระบบติดตามการจัดหา<br>ยาเฉพาะรายให้ผู้ป่วย</h5>
+    <small class="text-pharm">งานเภสัชกรรม โรงพยาบาลศรีนครินทร์</small></div>
     <div class="list-group list-group-flush">
-        <a href="../home/" class="list-group-item list-group-item-action sidenavGreen">สถานะเอกสาร</a>
-        <a href="../new/" class="list-group-item list-group-item-action sidenavGreen">ยื่นเรื่องใหม่</a>
+        <a href="../status/" class="list-group-item list-group-item-action sidenavGreen">ติดตามสถานะเอกสาร</a>
+        <a href="../form/" class="list-group-item list-group-item-action sidenavGreen">กรอกแบบฟอร์มขอใช้ยา</a>
         <!--?php if (isAdmin()) { ?-->
         <a href="../list/" class="list-group-item list-group-item-action sidenavGreen">รายการเอกสาร</a>
         <a href="../task/" class="list-group-item list-group-item-action sidenavGreen">เอกสารรอรับรอง</a>
